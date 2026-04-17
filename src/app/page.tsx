00c1e19b6a47700b4c017384ba85213a197dc885
godetremy <godetremy@free.fr>
2026-04-17 15:27:56 +0200

    fix: unused style and prisma import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { generateFortyTwoAuthorizationUrl } from '@/rest/fortytwo';
 import './page.css';
 import Image from 'next/image';
-import styles from './page.module.css';
 
 export default function Home() {
 	return (
