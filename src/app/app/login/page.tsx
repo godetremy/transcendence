58f03046680ce463ea615744c0d3147d6d7be27f
godetremy <godetremy@free.fr>
2026-04-20 16:59:14 +0200

    feat: create base routes

---
+++ src/app/app/login/page.tsx
@@ -0,0 +1,35 @@
+import { generateFortyTwoAuthorizationUrl } from '@/rest/fortytwo';
+import '../../page.css';
+import Image from 'next/image';
+
+export default function Page() {
+	return (
+		<div className="main">
+			<div className="login">
+				<Image src="/login/eyes_icon.svg" alt="eyes icon" width={73} height={70} />
+
+				<div>
+					<h1>CONNEXION</h1>
+					<p>Pour accéder à tes services connecte toi avec 42.</p>
+				</div>
+
+				<div className="login-btn">
+					<div>
+						<a className="login-42" href={generateFortyTwoAuthorizationUrl()}>
+							<Image src="/login/42.png" alt="logo 42" width={28} height={15}></Image>
+							<p>Continuer avec 42</p>
+						</a>
+					</div>
+					<div className="other-btn">
+						<a>
+							<p>Vous êtes un agents extérieur ?</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="image-container">
+				<Image src="/login/bg.png" alt="the image background" className="img-bg" width={640} height={832} />
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,10 @@
-import { generateFortyTwoAuthorizationUrl } from '@/rest/fortytwo';
 import './page.css';
-import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<div className="main">
-			<div className="login">
-				<Image src="/login/eyes_icon.svg" alt="eyes icon" width={73} height={70} />
-
-				<div>
-					<h1>CONNEXION</h1>
-					<p>Pour accéder à tes services connecte toi avec 42.</p>
-				</div>
-
-				<div className="login-btn">
-					<div>
-						<a className="login-42" href={generateFortyTwoAuthorizationUrl()}>
-							<Image src="/login/42.png" alt="logo 42" width={28} height={15}></Image>
-							<p>Continuer avec 42</p>
-						</a>
-					</div>
-					<div className="other-btn">
-						<a>
-							<p>Vous êtes un agents extérieur ?</p>
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="image-container">
-				<Image src="/login/bg.png" alt="the image background" className="img-bg" width={640} height={832} />
-			</div>
-		</div>
+		<main>
+			<h1>Y&apos;a plus rien ici pour l&apos;instant</h1>
+			<a href={'/app/login'}>Login</a>
+		</main>
 	);
 }
