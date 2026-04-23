519609eadabe0a9a55f4b6ee75f24664849c0d2e
godetremy <godetremy@free.fr>
2026-04-23 14:18:20 +0200

    feat: loginTemplate component

--- src/app/app/login/page.tsx
+++ src/app/app/login/page.tsx
@@ -6,6 +6,7 @@ import { StudentLoginPagesImages } from '@/const/StudentLoginPagesImages';
 import { generateFortyTwoAuthorizationUrl } from '@/rest/fortytwo';
 import { Eyes } from '@/components/stickers/eyes/Eyes';
 import { FortyTwo } from '@/components/stickers/FortyTwo/FortyTwo';
+import { LoginTemplate } from '@/components/login/loginTemplate/LoginTemplate';
 
 export default function Page() {
 	const [image] = useState(() => {
@@ -13,28 +14,28 @@ export default function Page() {
 	});
 
 	return (
-		<main>
-			<section className={'content'}>
-				<Eyes className={'stickers'} />
+		<LoginTemplate
+			background={{
+				source: image.source,
+				alt: image.alt,
+			}}
+		>
+			<Eyes className={'stickers'} />
 
-				<div className={'text'}>
-					<h1>Connexion</h1>
-					<p>Pour accéder à tes services connecte toi avec 42.</p>
-				</div>
+			<div className={'text'}>
+				<h1>Connexion</h1>
+				<p>Pour accéder à tes services connecte toi avec 42.</p>
+			</div>
 
-				<div className={'actions'}>
-					<a href={generateFortyTwoAuthorizationUrl()} className={'primary'}>
-						<FortyTwo className={'icon'} />
-						Connexion avec 42
-					</a>
-					<a href={'/app/login/agents'} className={'secondary'}>
-						Vous êtes un agents extérieur ?
-					</a>
-				</div>
-			</section>
-			<section className={'background'}>
-				<Image src={image.source} alt={image.alt} className={'background_img'} fill />
-			</section>
-		</main>
+			<div className={'actions'}>
+				<a href={generateFortyTwoAuthorizationUrl()} className={'primary'}>
+					<FortyTwo className={'icon'} />
+					Connexion avec 42
+				</a>
+				<a href={'/app/login/agents'} className={'secondary'}>
+					Vous êtes un agents extérieur ?
+				</a>
+			</div>
+		</LoginTemplate>
 	);
 }
