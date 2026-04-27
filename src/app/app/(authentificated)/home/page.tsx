bb8b6f1284e6ff04ce450f1cf71b450be13ddd05
godetremy <godetremy@free.fr>
2026-04-27 21:48:08 +0200

    feat: sidebar / tabbar layout set in layout

--- src/app/app/(authentificated)/home/page.tsx
+++ src/app/app/(authentificated)/home/page.tsx
@@ -28,37 +28,53 @@ export default function Page() {
 
 	return (
 		<>
-			<p>Hello, World home !</p>
-			{user === null ? (
-				<p>Loading...</p>
-			) : (
-				<>
-					<p>first name : {user.first_name}</p>
-					<p>last name : {user.last_name}</p>
-					<p>full name : {user.full_name}</p>
-					<p>mail : {user.mail}</p>
-					<p>id : {user.id}</p>
-					<p>is agent : {user.is_agent ? 'true' : 'false'}</p>
-					<p>memberships id : {user.memberships_id ?? '-'}</p>
-					<p>start at : {user.memberships?.start_at.toLocaleDateString() ?? '-'}</p>
-					<p>end at : {user.memberships?.end_at.toLocaleDateString() ?? '-'}</p>
-				</>
-			)}
-			<button
-				onClick={() => {
-					logout();
+			<div
+				style={{
+					backgroundImage: 'url(/images/demo_profile.jpg)',
+					backgroundSize: 'cover',
+					backgroundOrigin: 'center',
+					height: 300,
+					display: 'flex',
+					flexDirection: 'column',
 				}}
 			>
-				log out
-			</button>
-			<p>or</p>
-			<button
-				onClick={() => {
-					deleteAccount();
-				}}
-			>
-				delete account
-			</button>
+				<div className={'content'}>
+					<h1>Hello, world !</h1>
+				</div>
+			</div>
+			<section className={'content'}>
+				{user === null ? (
+					<p>Loading...</p>
+				) : (
+					<>
+						<p>first name : {user.first_name}</p>
+						<p>last name : {user.last_name}</p>
+						<p>full name : {user.full_name}</p>
+						<p>mail : {user.mail}</p>
+						<p>id : {user.id}</p>
+						<p>is agent : {user.is_agent ? 'true' : 'false'}</p>
+						<p>memberships id : {user.memberships_id ?? '-'}</p>
+						<p>start at : {user.memberships?.start_at.toLocaleDateString() ?? '-'}</p>
+						<p>end at : {user.memberships?.end_at.toLocaleDateString() ?? '-'}</p>
+					</>
+				)}
+				<button
+					onClick={() => {
+						logout();
+					}}
+				>
+					log out
+				</button>
+				<p>or</p>
+				<button
+					onClick={() => {
+						deleteAccount();
+					}}
+				>
+					delete account
+				</button>
+				<div style={{ height: 10000, background: 'red' }} />
+			</section>
 		</>
 	);
 }
