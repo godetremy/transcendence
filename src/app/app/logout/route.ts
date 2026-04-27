cf5bfd156653695c37e02428dac23bdcbdb91373
amaury <amaury.blanchet37@gmail.com>
2026-04-27 13:53:14 -0400

    fix: delete function deletecookie

--- src/app/app/logout/route.ts
+++ src/app/app/logout/route.ts
@@ -1,12 +1,22 @@
-import { deleteCookie } from '@/lib/cookie';
 import { decrypt } from '@/lib/session';
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
 	try {
 		await decrypt(req.cookies.get('session')?.value);
-		await deleteCookie('session');
+		const cookieStore = await cookies();
+		const cookie = cookieStore.get('session');
+		if (cookie != null) {
+			cookieStore.set('session', cookie.value, {
+				httpOnly: true,
+				secure: true,
+				expires: Date.now(),
+				sameSite: 'lax',
+				path: '/',
+			});
+		}
 	} catch (error: unknown) {
 		console.error(error);
 		return new NextResponse(`Failed to logout. Please try again later.`, {
