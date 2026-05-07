6755a44aa6c497fc425c1506cb88794df4af9dc6
Manouill3 <manuarii.degache@gmail.com>
2026-05-07 14:24:07 +0200

    fix: fix proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import { SessionPayload } from '@/types/session/SessionPayload';
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 import { NextRequest, NextResponse } from 'next/server';
 
-const ignorePath = ['/app/api/auth/oauth', '/app/api/users/me'];
+const ignorePath = ['/app/api/auth/oauth'];
 
 function isPathIgnored(path: string): boolean {
 	if (!path.startsWith('/app')) return true;
