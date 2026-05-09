c5b528d3dad72dfdb38461db92a67cdebc1d59fd
amaury <amaury.blanchet37@gmail.com>
2026-05-09 12:43:42 -0400

    fix: setusername and setuserreason

--- src/app/app/(authentificated)/agents/approval/page.tsx
+++ src/app/app/(authentificated)/agents/approval/page.tsx
@@ -1,28 +1,42 @@
 'use client';
+import { setUserName, setUserReason } from '@/database/users/setUser';
+import { useState } from 'react';
 
 export default function Page() {
+	const [responseName, setResponseName] = useState<string>('');
+	const [responseReason, setResponseReason] = useState<string>('');
+
 	const validName = async (form: FormData) => {
-		fetch('/app/api/users/me/name', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/js',
-			},
-			body: JSON.stringify({
-				name: form.get('name'),
-			}),
-		});
+		const value = form.get('name');
+		if (value != null) {
+			const status = await setUserName(value.toString());
+			setResponseName(status);
+		} else {
+			setResponseName('the field is empty');
+		}
+	};
+
+	const validReason = async (form: FormData) => {
+		const value = form.get('reason');
+		if (value != null) {
+			const status = await setUserReason(value.toString());
+			setResponseReason(status);
+		} else {
+			setResponseReason('the field is empty');
+		}
 	};
 	return (
 		<>
 			<form action={validName}>
 				<p>Nom de l agent :</p>
-				<input type="text" placeholder="Nom" name="name" />
+				<input type="text" placeholder="Nom" name="name" required={true} />
 			</form>
-			<form>
+			{responseName && <p>{responseName}</p>}
+			<form action={validReason}>
 				<p>raison :</p>
-				<input type="text"></input>
-				<button>valider raison</button>
+				<input type="text" placeholder="Reason" name="reason" required={true}></input>
 			</form>
+			{responseReason && <p>{responseReason}</p>}
 			<div>
 				<button>valider compte</button>
 				<button>refuser compte</button>
--- src/database/users/setUser.ts
+++ src/database/users/setUser.ts
@@ -1,15 +1,36 @@
+'use server';
 import { prisma } from '@/database/prisma/prisma';
+import { decrypt } from '@/lib/session';
+import { cookies } from 'next/headers';
 
-export async function setUserName(user_id: string, name: string) {
-	await prisma.users.update({
-		where: { id: user_id },
-		data: { full_name: name },
-	});
+export async function setUserName(name: string): Promise<string> {
+	try {
+		const cookieStore = await cookies();
+		const cookie = cookieStore.get('session');
+		const session = await decrypt(cookie?.value);
+		await prisma.users.update({
+			where: { id: session.user_id },
+			data: { full_name: name },
+		});
+	} catch (error: unknown) {
+		console.error(error);
+		return 'Error, failed to set agent name.';
+	}
+	return 'Agent name set';
 }
 
-export async function setUserReason(user_id: string, reason: string) {
-	await prisma.users.update({
-		where: { id: user_id },
-		data: { reason: reason },
-	});
+export async function setUserReason(reason: string): Promise<string> {
+	try {
+		const cookieStore = await cookies();
+		const cookie = cookieStore.get('session');
+		const session = await decrypt(cookie?.value);
+		await prisma.users.update({
+			where: { id: session.user_id },
+			data: { reason: reason },
+		});
+	} catch (error: unknown) {
+		console.error(error);
+		return 'Error, failed to set agent reason.';
+	}
+	return 'Agent reason set';
 }
