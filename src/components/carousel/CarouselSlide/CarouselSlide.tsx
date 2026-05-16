32e4601e0ce12caa7c0548da2f720157b7baf008
godetremy <godetremy@free.fr>
2026-05-16 18:18:52 +0200

    feat: carousel reponsive

--- src/components/carousel/CarouselSlide/CarouselSlide.tsx
+++ src/components/carousel/CarouselSlide/CarouselSlide.tsx
@@ -15,7 +15,14 @@ export interface CarouselSlideProps extends SlideProps, ComponentPropsWithoutRef
 	progression: number;
 }
 
-export function CarouselSlide(props: CarouselSlideProps) {
+export function CarouselSlide({
+	slideImage,
+	slideTitle,
+	slideDescription,
+	slideTag,
+	progression,
+	...props
+}: CarouselSlideProps) {
 	return (
 		<div
 			{...props}
@@ -23,43 +30,43 @@ export function CarouselSlide(props: CarouselSlideProps) {
 			aria-live={'off'}
 			className={styles.slide}
 			style={{
-				backgroundImage: `url(${props.slideImage})`,
-				backgroundSize: `${120 - props.progression * 20}%`,
-				opacity: Math.max(props.progression, 0.5),
+				backgroundImage: `url(${slideImage})`,
+				backgroundSize: `${120 - progression * 20}%`,
+				opacity: Math.max(progression, 0.5),
 				...props.style,
 			}}
 		>
-			<div
+			<button
 				className={styles.content}
 				style={{
-					backdropFilter: `blur(${2 - props.progression * 2}px) saturate(0%) brightness(0.7)`,
+					backdropFilter: `blur(${2 - progression * 2}px) saturate(0%) brightness(0.7)`,
 				}}
 			>
 				<span
 					style={{
-						clipPath: `inset(0 ${100 - props.progression * 100}% 0 0)`,
-						backgroundColor: `var(--color-primary-${props.slideTag.color}`,
+						clipPath: `inset(0 ${100 - progression * 100}% 0 0)`,
+						backgroundColor: `var(--color-primary-${slideTag.color}`,
 					}}
 				>
-					{props.slideTag.text}
+					{slideTag.text}
 				</span>
 				<h3
 					style={{
-						opacity: props.progression,
-						transform: `translateX(${50 - props.progression * 50}px)`,
+						opacity: progression,
+						transform: `translateX(${50 - progression * 50}px)`,
 					}}
 				>
-					{props.slideTitle}
+					{slideTitle}
 				</h3>
 				<p
 					style={{
-						opacity: props.progression,
-						transform: `translateX(${100 - props.progression * 100}px)`,
+						opacity: progression,
+						transform: `translateX(${100 - progression * 100}px)`,
 					}}
 				>
-					{props.slideDescription}
+					{slideDescription}
 				</p>
-			</div>
+			</button>
 		</div>
 	);
 }
