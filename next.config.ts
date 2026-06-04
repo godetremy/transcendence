e8d6151a5eddf3fe53ab21924fd0e635a0bda270
godetremy <godetremy@free.fr>
2026-06-04 18:23:52 +0200

    fix: oops

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [new URL('https://cdn.intra.42.fr/**')],
 	},
-	allowedDevOrigins: ['10.18.242.72', 'nanci-windswept-lingeringly.ngrok-free.dev'],
 };
 
 export default withMDX(nextConfig);
