7bb537a1dd6aec37f84efd6a31eca96ed3910579
godetremy <godetremy@free.fr>
2026-06-05 18:18:16 +0200

    fix: unused error in this case

--- src/schema/AgentsSignInParametersSchema.ts
+++ src/schema/AgentsSignInParametersSchema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const AgentsSignInParametersSchema = z.object({
-	mail: z.email({ error: 'mail' }).trim(),
-	password: z.string({ error: 'password' }).trim(),
+	mail: z.email().trim(),
+	password: z.string().trim(),
 });
