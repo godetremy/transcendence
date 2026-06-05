b4a2bc9617ac76d9357b3f60a38bf24e92728223
godetremy <godetremy@free.fr>
2026-06-05 13:11:43 +0200

    feat: add errors utils to have uniform errors messages

---
+++ src/utils/errors.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+import { ApiError } from '@/types/ApiError';
+
+const ERRORS_DETAILS = {
+	unsupported_content_type: () => 'Unsupported content type.',
+	invalid_body: () => 'Invalid body.',
+	missing_parameter: (parameter: string) => `Missing required parameters ${parameter}.`,
+	invalid_parameter: (parameter: string) => `Invalid parameter ${parameter}.`,
+	internal_error: () => 'Internal server error',
+	invalid_mail_password: () => 'Invalid email or password',
+	password_not_set: () => "It's look like your password is not set. Are you a student ?",
+	account_already_exists: () => 'This account already exists.',
+};
+
+const apiError = (message?: string, status?: number): NextResponse => {
+	const content: ApiError = { success: false };
+
+	if (message !== undefined) {
+		content.message = message;
+	}
+
+	return NextResponse.json(content, { status: status ?? 500 });
+};
+
+const serverError = (error: unknown, status?: number): NextResponse => {
+	console.error(error);
+	return NextResponse.json({ success: false, message: ERRORS_DETAILS.internal_error }, { status: status ?? 500 });
+};
+
+export { apiError, serverError, ERRORS_DETAILS };
