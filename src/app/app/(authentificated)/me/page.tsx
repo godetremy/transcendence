1422fe3fe060bf58fe339066a5f96d5149439ce0
godetremy <godetremy@free.fr>
2026-06-09 10:01:23 +0200

    fix: close modal on logout

--- src/app/app/(authentificated)/me/page.tsx
+++ src/app/app/(authentificated)/me/page.tsx
@@ -77,7 +77,10 @@ export default function Page() {
 									{
 										text: 'Se déconnecter',
 										negative: true,
-										onClick: () => router.push('/app/api/auth/logout'),
+										onClick: () => {
+											closeModal();
+											router.push('/app/api/auth/logout');
+										},
 									},
 								],
 							});
