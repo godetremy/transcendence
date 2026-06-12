dce00a93f535318c1ad39278ef6a78f0f3f2d579
momori85 <177555279+momori85@users.noreply.github.com>
2026-06-12 13:27:12 +0200

    feat: list users registered to event

--- src/app/app/api/events/[event_id]/subscribe/route.ts
+++ src/app/app/api/events/[event_id]/subscribe/route.ts
@@ -1,15 +1,18 @@
 import { getEventById } from '@/database/Event';
+import { formatPublicRegisteredEvent } from '@/database/format/RegisteredEvent';
 import { registered_eventWhereUniqueInput } from '@/database/prisma/generated/models';
 import {
 	countRegisteredEventsByFilter,
 	createRegisteredEventById,
 	deleteRegisteredEventById,
 	getRegisteredEventById,
+	getRegistersToEventById,
 } from '@/database/RegisteredEvent';
 import { decrypt } from '@/lib/session';
 import { RegisteredEventParamSchema } from '@/schema/RegisteredEventSchema';
 import { RegisteredEventParam } from '@/types/RegisteredEvent';
 import { apiError, ERRORS_DETAILS, serverError } from '@/utils/errors';
+import { generatePaginationResponse, getPaginationParams } from '@/utils/pagination';
 import { parseBody } from '@/utils/parsing';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -50,6 +53,31 @@ export async function PATCH(
 		}
 
 		return NextResponse.json({ success: true });
+	} catch (err: unknown) {
+		if (typeof err === 'string') return apiError(err, 400);
+		return serverError(err);
+	}
+}
+
+export async function GET(
+	req: NextRequest,
+	{ params }: { params: Promise<{ event_id: string }> }
+): Promise<NextResponse> {
+	try {
+		const { event_id } = await params;
+
+		
+		const event = await getEventById(event_id, { registered: true });
+		if (event == null) throw ERRORS_DETAILS.event_does_not_exists();
+
+		const pagination = getPaginationParams(req.nextUrl.searchParams);
+		const count = await countRegisteredEventsByFilter({ registered_event_id: event_id });
+
+		const list = await getRegistersToEventById({event: true}, event_id, pagination);
+		console.error(list);
+		
+		return NextResponse.json(generatePaginationResponse(list.map(formatPublicRegisteredEvent), count, pagination));
+
 	} catch (err: unknown) {
 		if (typeof err === 'string') return apiError(err, 400);
 		return serverError(err);
--- src/database/RegisteredEvent.ts
+++ src/database/RegisteredEvent.ts
@@ -1,5 +1,7 @@
+import { DEFAULT_PAGINATION, paginationToPrisma } from '@/utils/pagination';
 import { Prisma } from './prisma/generated/client';
 import { prisma } from './prisma/prisma';
+import { PaginationParameters } from '@/types/PaginationParameters';
 
 const countRegisteredEventsByFilter = async (filter?: Prisma.registered_eventWhereInput): Promise<number> => {
 	return prisma.registered_event.count({
@@ -45,4 +47,19 @@ const deleteRegisteredEventById = async <T extends Prisma.registered_eventInclud
 	});
 };
 
-export { countRegisteredEventsByFilter, getRegisteredEventById, createRegisteredEventById, deleteRegisteredEventById };
+const getRegistersToEventById = async <T extends Prisma.registered_eventInclude>(
+	include: T,
+	event_id: string,
+	pagination?: PaginationParameters
+): Promise<Prisma.registered_eventGetPayload<{ include: T }>[]> => {
+	const value = await prisma.registered_event.findMany({
+		include: include,
+		where: {
+			registered_event_id: event_id,
+		},
+		...paginationToPrisma(pagination ?? DEFAULT_PAGINATION),
+	});
+	return value;
+};
+
+export { countRegisteredEventsByFilter, getRegisteredEventById, createRegisteredEventById, deleteRegisteredEventById, getRegistersToEventById };
