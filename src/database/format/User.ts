340e11e0d5ca55c2a0ff1e68508101ed083375b2
godetremy <godetremy@free.fr>
2026-06-15 21:07:44 +0200

    feat: some prisma little modification
    Co-authored-by: amaury <amaury.blanchet37@gmail.com>

--- src/database/format/User.ts
+++ src/database/format/User.ts
@@ -5,7 +5,7 @@ import formatMembership from '@/database/format/Membership';
 const formatPrivateUser = <T extends Prisma.usersInclude>(row: Prisma.usersGetPayload<{ include: T }>): User => {
 	// This filter private database data.
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const { fortytwo_oauth_id, fortytwo_user_id, password, agent_verified, ...user } = row;
+	const { fortytwo_oauth_id, fortytwo_user_id, password, two_factor_auth_id, memberships_id, ...user } = row;
 
 	return {
 		...user,
@@ -26,6 +26,9 @@ const formatPublicUser = (row: Prisma.usersGetPayload<object>): PublicUser => {
 		full_name: row.full_name,
 		profile_picture: row.full_name,
 		agent: row.agent,
+		created_at: row.created_at,
+		updated_at: row.updated_at,
+		is_member: row.memberships_id !== null,
 	};
 };
 
