19cda7aa31a09c0513abb2ceb8443eb0b615db6b
Pokalie566 <aubindebooserepros@gmail.com>
2026-06-23 14:13:27 +0200

    feat: add docker compose files for postgres and seaweedfs services

--- docker/scripts/generate_environnement.sh
+++ docker/scripts/generate_environnement.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-source ./docker/scripts/helper.sh;
+source "./docker/scripts/helper.sh"
 
 if [ -f "$DEVELOPMENT_ENV" ]||[ -f "$STAGING_ENV" ]||[ -f "$PRODUCTION_ENV" ]; then
-	print_warning 'environment file found.'
+    print_warning 'environment file found.'
     confirm "Reinitialize them ? [Y/n]"
 fi
 
@@ -14,7 +14,8 @@ export PRODUCTION_URL="$URL"
 
 DATABASE_USERNAME=$(read_with_prompt "Which username do you want to use for your database" "user");
 DATABASE_PASSWORD=$(read_password "Which password do you want to use for your database");
-DATABASE_NAME=$(read_with_prompt "Which name do you want to use for your database", "transcendence");
+DATABASE_NAME=$(read_with_prompt "Which name do you want to use for your database" "transcendence");
+DATABASE_PORT=5432
 
 export DEVELOPMENT_DATABASE_NAME="dev_$DATABASE_NAME"
 export STAGING_DATABASE_NAME="staging_$DATABASE_NAME"
@@ -25,8 +26,9 @@ STAGING_SESSION_SECRET=$(read_password "Which secret do you want to use for your
 PRODUCTION_SESSION_SECRET=$(read_password "Which secret do you want to use for your session on production deploy");
 
 echo
-source ./docker/scripts/request_forty_two_api.sh
+source "$SCRIPT_DIR/request_forty_two_api.sh"
 echo
+
 
 BASE_ENV_CONTENT="# This env has been generated automatically.
 
@@ -36,11 +38,17 @@ OAUTH_42_SECRET=$FORTYTWO_CLIENT_SECRET
 DATABASE_PORT=5432
 DATABASE_USERNAME=$DATABASE_USERNAME
 DATABASE_PASSWORD=$DATABASE_PASSWORD
+USER_POSTGRES=$DATABASE_USERNAME
+PASSWORD_POSTGRES=$DATABASE_PASSWORD
+HOST=127.0.0.1
+PORT_POSTGRES=$DATABASE_PORT
 "
 
 DEVELOPMENT_ENV_CONTENT="
 $BASE_ENV_CONTENT
 DATABASE_NAME=$DEVELOPMENT_DATABASE_NAME
+DB_NAME_POSTGRES=$DEVELOPMENT_DATABASE_NAME
+DATABASE_URL=postgresql://$DATABASE_USERNAME:$DATABASE_PASSWORD@$HOST:$DATABASE_PORT/$DEVELOPMENT_DATABASE_NAME
 
 NEXT_PUBLIC_BASE_URL=$DEVELOPMENT_URL
 
@@ -50,6 +58,9 @@ SESSION_SECRET=$DEVELOPMENT_SESSION_SECRET
 STAGING_ENV_CONTENT="
 $BASE_ENV_CONTENT
 DATABASE_NAME=$STAGING_DATABASE_NAME
+DB_NAME_POSTGRES=$STAGING_DATABASE_NAME
+# Node conteneurisé : il joint postgres par le nom de service compose, pas 127.0.0.1
+DATABASE_URL=postgresql://$DATABASE_USERNAME:$DATABASE_PASSWORD@postgres:$DATABASE_PORT/$STAGING_DATABASE_NAME
 
 NEXT_PUBLIC_BASE_URL=$STAGING_URL
 
@@ -59,6 +70,9 @@ SESSION_SECRET=$STAGING_SESSION_SECRET
 PRODUCTION_ENV_CONTENT="
 $BASE_ENV_CONTENT
 DATABASE_NAME=$PRODUCTION_DATABASE_NAME
+DB_NAME_POSTGRES=$PRODUCTION_DATABASE_NAME
+# Node conteneurisé : il joint postgres par le nom de service compose, pas 127.0.0.1
+DATABASE_URL=postgresql://$DATABASE_USERNAME:$DATABASE_PASSWORD@postgres:$DATABASE_PORT/$PRODUCTION_DATABASE_NAME
 
 NEXT_PUBLIC_BASE_URL=$PRODUCTION_URL
 
