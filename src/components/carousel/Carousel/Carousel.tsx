68badd3fb1b1c55f6d4a56b6b8677f974110a187
Timothy <timcybak@gmail.com>
2026-06-24 16:08:51 +0200

    fix: carrousel with pad or mouse when you go outside of the carrousel bug fix

--- src/components/carousel/Carousel/Carousel.tsx
+++ src/components/carousel/Carousel/Carousel.tsx
@@ -165,7 +165,6 @@ export function Carousel(props: CarouselProps) {
 
 	function dragEventHandler(event: MouseEvent) {
 		if (event.buttons !== 1) return;
-
 		slideTimeoutPaused.current = true;
 
 		const container = slidesContainerRef.current;
@@ -192,7 +191,6 @@ export function Carousel(props: CarouselProps) {
 	function endDragEventHandler() {
 		const container = slidesContainerRef.current;
 		if (!container) return;
-
 		slideTimeoutPaused.current = false;
 		updateSlideTimeout();
 
@@ -200,6 +198,7 @@ export function Carousel(props: CarouselProps) {
 		const translateX = parseTranslateX(container);
 
 		const targetSlide = Math.round(-(translateX / slideWidth)) % container.children.length;
+		console.log({ targetSlide });
 		goToSlide(targetSlide);
 	}
 
@@ -230,7 +229,7 @@ export function Carousel(props: CarouselProps) {
 			window.addEventListener('resize', resizeEventHandler);
 			container?.addEventListener('mousedown', beginDragEventHandler);
 			container?.addEventListener('mousemove', dragEventHandler);
-			container?.addEventListener('mouseup', endDragEventHandler);
+			window.addEventListener('mouseup', endDragEventHandler);
 			container?.addEventListener('touchstart', beginDragTouchEventHandler);
 			container?.addEventListener('touchmove', dragTouchEventHandler);
 			container?.addEventListener('touchend', endDragTouchEventHandler);
@@ -239,7 +238,7 @@ export function Carousel(props: CarouselProps) {
 			window.removeEventListener('resize', resizeEventHandler);
 			container?.removeEventListener('mousedown', beginDragEventHandler);
 			container?.removeEventListener('mousemove', dragEventHandler);
-			container?.removeEventListener('mouseup', endDragEventHandler);
+			window.removeEventListener('mouseup', endDragEventHandler);
 			container?.removeEventListener('touchstart', beginDragTouchEventHandler);
 			container?.removeEventListener('touchmove', dragTouchEventHandler);
 			container?.removeEventListener('touchend', endDragTouchEventHandler);
@@ -258,7 +257,7 @@ export function Carousel(props: CarouselProps) {
 		}, 100);
 		createEventHandler();
 		return removeEventHandler;
-	}, []);
+	});
 
 	return (
 		<div role={'group'} aria-roledescription={'carousel'} className={styles.carousel} ref={carouselRef}>
