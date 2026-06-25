2015e82eb8d1fe49397f13606e47fc72f611322b
momori85 <177555279+momori85@users.noreply.github.com>
2026-06-25 11:09:41 +0200

    fix(back): delete member

--- src/app/app/api/organization/[org_id]/members/[user_id]/route.ts
+++ src/app/app/api/organization/[org_id]/members/[user_id]/route.ts
@@ -87,7 +87,7 @@ export async function DELETE(
 
 		const user = await getOrganizationMemberById(id.id, org_id, {});
 		if (user === null) throw ERRORS_DETAILS.member_not_in_organization();
-		if (session.user_id === organization.owner_id) throw ERRORS_DETAILS.cant_leave_as_owner();
+		if (user.id === organization.owner_id) throw ERRORS_DETAILS.cant_leave_as_owner();
 		const member = await deleteMemberFromOrganization(id.id, org_id);
 		if (!member) throw ERRORS_DETAILS.member_not_in_organization();
 
