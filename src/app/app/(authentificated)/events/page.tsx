34db2ed023bfab01d664397125fff849ef9c98ba
Timothy <timcybak@gmail.com>
2026-07-09 13:35:10 +0200

    feat: page event recherche barre

--- src/app/app/(authentificated)/events/page.tsx
+++ src/app/app/(authentificated)/events/page.tsx
@@ -1,13 +1,52 @@
-import { Calendar } from '@/components/globals/Calendar/Calendar';
 import styles from './page.module.scss';
 import { EventPreview } from '@/components/globals/EventPreview/EventPreview';
+import { Search } from 'lucide-react';
 
 export default function Page() {
 	return (
 		<>
 			<div className={styles.page}>
-				<p>Hello, World !</p>
-				<Calendar date={1} />
+				<div className={styles.groupButton}>
+					<div className={styles.scrollArea}>
+						<button className={styles.first}>Tous</button>
+						<button className={styles.button}>Inscrit</button>
+						<button className={styles.button}>Par les clubs</button>
+						<button className={styles.button}>Par les clubs</button>
+					</div>
+					<button className={styles.buttonSearch}>
+						<Search size={16} />
+					</button>
+				</div>
+				<h1 className={styles.h1}>AUJOURD'HUI</h1>
+				<EventPreview
+					image={'/images/demo_event_01.png'}
+					date={12}
+					title={'coucou'}
+					time={'Lundi de 12h a 15h'}
+					location={'Terrasse'}
+				/>
+				<EventPreview
+					image={'/images/demo_event_01.png'}
+					date={12}
+					title={'coucou'}
+					time={'Lundi de 12h a 15h'}
+					location={'Terrasse'}
+				/>
+				<EventPreview
+					image={'/images/demo_event_01.png'}
+					date={12}
+					title={'coucou'}
+					time={'Lundi de 12h a 15h'}
+					location={'Terrasse'}
+				/>
+				<h1 className={styles.h1}>DEMAIN</h1>
+				<EventPreview
+					image={'/images/demo_event_01.png'}
+					date={12}
+					title={'coucou'}
+					time={'Lundi de 12h a 15h'}
+					location={'Terrasse'}
+				/>
 				<EventPreview
 					image={'/images/demo_event_01.png'}
 					date={12}
