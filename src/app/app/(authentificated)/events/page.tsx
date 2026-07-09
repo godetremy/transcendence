de9f7596e670811cf5d0a2f0f45d4065374ea17f
Timothy <timcybak@gmail.com>
2026-07-09 10:06:11 +0200

    feat: calendar and composant for event

--- src/app/app/(authentificated)/events/page.tsx
+++ src/app/app/(authentificated)/events/page.tsx
@@ -1,7 +1,21 @@
+import { Calendar } from '@/components/globals/Calendar/Calendar';
+import styles from './page.module.scss';
+import { EventPreview } from '@/components/globals/EventPreview/EventPreview';
+
 export default function Page() {
 	return (
 		<>
-			<p>Hello, World !</p>
+			<div className={styles.page}>
+				<p>Hello, World !</p>
+				<Calendar date={1} />
+				<EventPreview
+					image={'/images/demo_event_01.png'}
+					date={12}
+					title={'coucou'}
+					time={'Lundi de 12h a 15h'}
+					location={'Terrasse'}
+				/>
+			</div>
 		</>
 	);
 }
