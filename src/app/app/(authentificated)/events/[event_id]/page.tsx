61ba9a584bfe0666e3b99f742d52232594555ca3
godetremy <godetremy@free.fr>
2026-07-16 10:17:00 +0200

    feat: markdown render and styling for event details page

--- src/app/app/(authentificated)/events/[event_id]/page.tsx
+++ src/app/app/(authentificated)/events/[event_id]/page.tsx
@@ -2,106 +2,121 @@
 import styles from './page.module.scss';
 import { useParams } from 'next/navigation';
 import { Calendar } from '@/components/globals/Calendar/Calendar';
-import { Clock, MapPin } from 'lucide-react';
+import { CalendarPlus, Clock, MapPin, Navigation } from 'lucide-react';
 import { motion, useScroll, useTransform } from 'framer-motion';
 import { useMediaQuery } from '@/contexts/MediaQueryProvider';
 import { useQuery } from '@tanstack/react-query';
 import { getEventPublic } from '@/lib/fetcher/events';
 import { Loader } from '@/components/globals/Loader/Loader';
 import { ErrorState } from '@/components/globals/ErrorState/ErrorState';
-import { getDay, getHours } from 'date-fns';
-
-const days = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+import ReactMarkdown from 'react-markdown';
+import { MenuButton } from '@/components/globals/MenuButton/MenuButton';
+import { toHumanReadablePeriod } from '@/utils/date';
+import remarkGfm from 'remark-gfm';
 
 export default function Page() {
-	const { event_id } = useParams();
+	const { event_id }: { event_id: string } = useParams();
 
-	const { data, isLoading, isError, error } = useQuery(getEventPublic(event_id as string));
+	const { data, isLoading, isError, error } = useQuery(getEventPublic(event_id));
+
+	const { scrollY } = useScroll();
 	const isMobile = useMediaQuery('(max-width: 768px)');
 	const headerSize = () => (isMobile ? 180 : 280);
-	const { scrollY } = useScroll();
 
-	const backgroundSize = useTransform(scrollY, [0, headerSize()], ['100%', '120%']);
-	const borderBottom = useTransform(scrollY, [headerSize() / 2, headerSize()], ['1px solid #000', '1px solid #222']);
+	const headerBackgroundSizeTransform = useTransform(scrollY, [0, headerSize()], ['100%', '120%']);
+	const headerBorderTransform = useTransform(
+		scrollY,
+		[headerSize() / 2, headerSize()],
+		['1px solid #000', '1px solid #222']
+	);
 
-	const translateY = useTransform(scrollY, [0, headerSize() * 0.75], [0, -20]);
-	const opacity = useTransform(scrollY, [headerSize() * 0.1, headerSize() * 0.75], [1, 0]);
-	const filter = useTransform(scrollY, [headerSize() * 0.1, headerSize() * 0.75], ['blur(0px)', 'blur(10px)']);
+	const headerDetailsTranslateTransform = useTransform(scrollY, [0, headerSize() * 0.75], [0, -20]);
+	const headerDetailsOpacityTransform = useTransform(scrollY, [headerSize() * 0.1, headerSize() * 0.75], [1, 0]);
+	const headerDetailsFilterTransform = useTransform(
+		scrollY,
+		[headerSize() * 0.1, headerSize() * 0.75],
+		['blur(0px)', 'blur(10px)']
+	);
 
-	const opacityNav = useTransform(scrollY, [headerSize() * 0.7, headerSize()], [0, 1]);
-	const pointerEvents = useTransform(scrollY, [headerSize() / 2, headerSize()], ['none', 'auto']);
+	const headerBarOpacityTransform = useTransform(scrollY, [headerSize() * 0.7, headerSize()], [0, 1]);
+	const headerBarPointerTransform = useTransform(scrollY, [headerSize() / 2, headerSize()], ['none', 'auto']);
 
-	const formatTime = (first: Date, second: Date) => {
-		const day = getDay(first);
-		if (day == getDay(second)) return `${days[day]} de ${getHours(first)}h à ${getHours(second)}h`;
-		return `${days[day]} à ${getHours(first)}h au ${days[getDay(second)]} à ${getHours(second)}h`;
-	};
+	if (isLoading) return <Loader />;
+	if (isError || data === undefined) return <ErrorState error={error} />;
 
 	return (
-		<>
-			{isLoading ? (
-				<Loader />
-			) : isError || data === undefined ? (
-				<ErrorState error={error} />
-			) : (
-				<div className={styles.main_container}>
-					<motion.header
-						style={{
-							backgroundImage: `url('/images/demo_event_01.png')`,
-							backgroundSize: backgroundSize,
-							borderBottom: borderBottom,
-						}}
-					>
-						<motion.div
-							className={styles.details}
-							style={{
-								translateY: translateY,
-								opacity: opacity,
-								filter: filter,
-							}}
-						>
-							<Calendar date={data.start_at} />
-							<div className={styles.text_container}>
-								<h1>{data.title}</h1>
-								<div className={styles.tags}>
-									<button>
-										<Clock size={14} />
-										<span>{formatTime(new Date(data.start_at), new Date(data.end_at))}</span>
-									</button>
-									<button>
-										<MapPin size={14} />
-										<span>{data.location}</span>
-									</button>
-								</div>
-							</div>
-						</motion.div>
-						<motion.div
-							className={styles.nav}
-							style={{
-								opacity: opacityNav,
-								pointerEvents: pointerEvents,
-							}}
-						>
-							<p>{data.title}</p>
-						</motion.div>
-					</motion.header>
-					<article>
-						<section>
-							<p>{data.description}</p>
-						</section>
-						<div style={{ height: 10000 }} />
-					</article>
-					<footer className={styles.cta_container}>
-						<motion.button
-							className={styles.cta_button}
-							whileHover={{ scale: 1.02 }}
-							whileTap={{ scale: 0.99 }}
-						>
-							S&#39;inscrire
-						</motion.button>
-					</footer>
-				</div>
-			)}
-		</>
+		<div className={styles.main_container}>
+			<motion.header
+				style={{
+					backgroundImage: `url('${data.image}')`,
+					backgroundSize: headerBackgroundSizeTransform,
+					borderBottom: headerBorderTransform,
+				}}
+			>
+				<motion.div
+					className={styles.details}
+					style={{
+						translateY: headerDetailsTranslateTransform,
+						opacity: headerDetailsOpacityTransform,
+						filter: headerDetailsFilterTransform,
+					}}
+				>
+					<Calendar date={data.start_at} />
+					<div className={styles.text_container}>
+						<h1>{data.title}</h1>
+						<div className={styles.tags}>
+							<MenuButton
+								containerKey={'event_tags'}
+								menu={[{ title: 'Ajouter au calendrier', icon: CalendarPlus, onClick: () => {} }]}
+							>
+								<Clock size={14} />
+								<span>{toHumanReadablePeriod(new Date(data.start_at), new Date(data.end_at))}</span>
+							</MenuButton>
+							{data.location && (
+								<MenuButton
+									containerKey={'location_tags'}
+									menu={[
+										{
+											title: 'Ouvrir dans Google Maps',
+											icon: Navigation,
+											onClick: () => {
+												window
+													.open(
+														`https://www.google.com/maps/search/?api=1&query=${encodeURI(data.location ?? '')}`,
+														'_blank'
+													)
+													?.focus();
+											},
+										},
+									]}
+									className={styles.tag}
+								>
+									<MapPin size={14} />
+									<span>{data.location}</span>
+								</MenuButton>
+							)}
+						</div>
+					</div>
+				</motion.div>
+				<motion.div
+					className={styles.nav}
+					style={{ opacity: headerBarOpacityTransform, pointerEvents: headerBarPointerTransform }}
+				>
+					<p>{data.title}</p>
+				</motion.div>
+			</motion.header>
+			<article>
+				{data.description && (
+					<section className={styles.markdown}>
+						<ReactMarkdown remarkPlugins={[remarkGfm]}>{data.description}</ReactMarkdown>
+					</section>
+				)}
+			</article>
+			<footer className={styles.cta_container}>
+				<motion.button className={styles.cta_button} whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.99 }}>
+					S&#39;inscrire
+				</motion.button>
+			</footer>
+		</div>
 	);
 }
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,8 @@
 import { DateOption } from '@/types/DateParameters';
 import { DateEventParamSchema } from '@/schema/EventSchema';
 import { parseParams } from './parsing';
+import { format } from 'date-fns';
+import { fr } from 'date-fns/locale';
 
 const DEFAULT_DATEOPTION: DateOption = {
 	from: new Date(Date.now() - 24 * 60 * 60 * 1000).toDateString(),
@@ -8,8 +10,7 @@ const DEFAULT_DATEOPTION: DateOption = {
 };
 
 const getDateParams = (params: URLSearchParams): DateOption => {
-	const data = parseParams<DateOption>(params, DateEventParamSchema);
-	return data;
+	return parseParams<DateOption>(params, DateEventParamSchema);
 };
 
 const dateToPrisma = (time: DateOption): { start_at?: { gte: Date }; end_at?: { lte: Date } } => {
@@ -31,4 +32,17 @@ const dateToPrisma = (time: DateOption): { start_at?: { gte: Date }; end_at?: { 
 	};
 };
 
-export { getDateParams, dateToPrisma, DEFAULT_DATEOPTION };
+const toHumanReadablePeriod = (from: Date, to: Date): string => {
+	const from_day = format(from, 'EEEE', { locale: fr });
+	const from_hour = format(from, 'HH', { locale: fr });
+	const from_minute = format(from, 'mm', { locale: fr });
+	const to_day = format(to, 'EEEE', { locale: fr });
+	const to_hour = format(to, 'HH', { locale: fr });
+	const to_minute = format(to, 'mm', { locale: fr });
+
+	if (from.getDate() === to.getDate() && from.getMonth() === to.getMonth() && from.getFullYear() === to.getFullYear())
+		return `${from_day.charAt(0).toUpperCase()}${from_day.slice(1)} de ${from_hour}h${from_minute} à ${to_hour}h${to_minute}`;
+	return `Du ${from_day} de ${from_hour}h${from_minute} au ${to_day} à ${to_hour}h${to_minute}`;
+};
+
+export { getDateParams, dateToPrisma, DEFAULT_DATEOPTION, toHumanReadablePeriod };
