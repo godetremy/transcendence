0a9f5a7c31f870353d5140bf4b02aaac2cffed08
Manouill3 <manuarii.degache@gmail.com>
2026-07-23 16:54:01 +0200

    feat: add filter to list of events

--- src/app/app/api/events/route.ts
+++ src/app/app/api/events/route.ts
@@ -4,10 +4,11 @@ import { formatPublicEvent } from '@/database/format/Event';
 import { Prisma } from '@/database/prisma/generated/client';
 import { SearchQuerySchema } from '@/schema/searchQuery';
 import { SearchQuery } from '@/types/searchQuery';
-import { errorHandler } from '@/utils/errors';
+import { errorHandler, ERRORS_DETAILS } from '@/utils/errors';
 import { generatePaginationResponse, getPaginationParams } from '@/utils/pagination';
 import { parseParams } from '@/utils/parsing';
 import { NextRequest, NextResponse } from 'next/server';
+import { getThrowableSession } from '@/lib/session';
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
 	return errorHandler(async () => {
@@ -15,6 +16,13 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
 
 		const pagination = getPaginationParams(searchParams);
 		const query = parseParams<SearchQuery>(searchParams, SearchQuerySchema);
+
+		const registered = searchParams.has('registered');
+		const club = searchParams.has('club');
+		const past = searchParams.has('past');
+
+		const user = await getThrowableSession(req);
+		if (!user) throw ERRORS_DETAILS.user_does_not_exists();
 
 		const searchs = await getEventsOrServicesByElasticSearch(query.q ?? '', pagination.limit, 'events');
 
@@ -31,6 +39,9 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
 				org_id: hit._source?.organization_id,
 				id: hit._source?.id,
 				title: hit._source?.title,
+				...(registered ? { event_registration: { some: { user_id: user.user_id } } } : {}),
+				...(club ? { organization: { club: true } } : {}),
+				...(past ? { start_at: { lt: new Date() } } : {}),
 				subtitle: hit._source?.subtitle,
 				description: hit._source?.description,
 			}));
@@ -50,6 +61,9 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
 							},
 						],
 					}),
+			...(registered ? { event_registration: { some: { user_id: user.user_id } } } : {}),
+			...(club ? { organization: { club: true } } : {}),
+			...(past ? { start_at: { lt: new Date() } } : {}),
 		};
 
 		const count = await countEventsByFilter(filter);
